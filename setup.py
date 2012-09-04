0345976aebcd02db977286d573010f25aae25f55
Tom Vincent <git@tlvince.com>
2012-09-04 16:41:44 +0800

    Add description to setup

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from distutils.core import setup
 
 setup(
     name = 'periodical',
-    scripts = ['src/periodical.py'],
+    description = 'Create a Kindle periodical from given URL(s)',
     version = '0.1.0',
     author = 'Tom Vincent',
     author_email = 'http://tlvince.com/contact/',
     url = 'https://github.com/tlvince/periodical',
     license = 'MIT',
+    scripts = ['src/periodical.py'],
     classifiers = [
         'Programming Language :: Python :: 2',
         'Intended Audience :: End Users/Desktop',
